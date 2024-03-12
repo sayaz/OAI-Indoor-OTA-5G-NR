b658386f36824bdea337d868894dae14498ad6de
Syed Ayaz Mahmud <sayazmahmud@gmail.com>
2024-03-12 09:28:44 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -403,7 +403,7 @@ cn_node.addService(rspec.Execute(shell="bash", command=cmd))
 
 
 # single x310 for gNB and UE for now
-b210_node_pair_gnb(0, params.b210_radio)
+b210_nuc_pair_gnb(0, params.b210_radio)
 
 # Single b210 for UE
 b210_nuc_pair_ue(0, params.b210_radio)
