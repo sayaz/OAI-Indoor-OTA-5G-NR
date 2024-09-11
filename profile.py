9d848215459a1eee35bbf57b3613653d01c432a8
Syed Ayaz Mahmud <sayazmahmud@gmail.com>
2024-09-11 09:32:14 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -526,7 +526,7 @@ b210_nuc_pair_gnb(0, params.b210_radio_gnb)
 # b210_nuc_pair_ue(2, params.b210_radio)
 
 # require all indoor OTA nucs for now
-for b210_node in ["ota-nuc1", "ota-nuc2"]:
+for b210_node in ["ota-nuc2"]:
     b210_nuc_pair_ue(b210_node)
 
 
