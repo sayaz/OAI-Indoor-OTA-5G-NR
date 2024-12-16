3ac3d1762b0d2ec9d706597c40e6a2450b42c45e
Syed Ayaz Mahmud <sayazmahmud@gmail.com>
2024-12-16 09:32:30 -0800

    Update profile_16.py

--- profile.py
+++ profile.py
@@ -625,8 +625,9 @@ if params.alloc_wifi:
     alloc_wifi_resources()
 
 # single x310 for gNB and UE for now
-x310_node_pair(1, params.x310_radio)
-# UE_node_x310(2, params.x310_radio_UE)
+x310_node_pair(0, params.x310_radio)
+UE_node_x310(1, params.x310_radio_UE)
+UE_node_x310(2, params.x310_radio_UE)
 
 
 # single b210 for gNB
@@ -639,9 +640,6 @@ b210_nuc_pair_gnb(0, params.b210_radio_gnb)
 # require all indoor OTA nucs for now
 for b210_node in ["ota-nuc2", "ota-nuc3", "ota-nuc4"]:
     b210_nuc_pair_ue(b210_node)
-
-for x310_node in ["ota-x310-2", "ota-x310-3", "ota-x310-4"]:
-    UE_node_x310(x310_node, params.x310_radio_UE)
 	
 for frange in params.freq_ranges:
     request.requestSpectrum(frange.freq_min, frange.freq_max, 0)
